e1920372fa873829fc721eedbe83fb1720470dca
akintolapraise134-create <akintolapraise134@gmail.com>
2026-05-28 09:19:39 +0100

    Add app configuration for MERCYLAND SCHOOLS

---
+++ mercyland_schools/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class MercylandSchoolsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mercyland_schools'
+    verbose_name = 'MERCYLAND SCHOOLS'
